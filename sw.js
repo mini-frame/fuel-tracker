dca80167695af0c84808fa9863efe589e83b7260
mini-frame <maddie.rainford@gmail.com>
2026-04-06 17:36:55 -0600

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,29 @@
+const CACHE = 'fuel-v1';
+const FILES = [
+  './fuel-tracker.html',
+  './manifest.json',
+  './fuel-icon.svg',
+  'https://fonts.googleapis.com/css2?family=DM+Sans:ital,opsz,wght@0,9..40,400;0,9..40,500;0,9..40,600;0,9..40,700;1,9..40,400&display=swap'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE).then(cache => cache.addAll(FILES))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(cached => cached || fetch(e.request))
+  );
+});
